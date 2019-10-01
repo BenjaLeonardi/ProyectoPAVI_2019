eabc102a33724f4e6ddd4e42582de402e99fa105
JulianBlane <jbenegaslane@gmail.com>
2019-10-01 19:09:05 -0300

    Inicio de diseño de la transaccion

---
+++ SistemaHotelPAV/SistemaHotelPAV/GUI/frmFactura.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SistemaHotelPAV.GUI {
+    partial class frmFactura {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipoFactura = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pickerFechaFactura = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNroEstadia = new System.Windows.Forms.TextBox();
+            this.txtNroFactura = new System.Windows.Forms.TextBox();
+            this.pickerFechaInicioEstadia = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.precioUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_articulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero de Factura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de Factura";
+            // 
+            // cmbTipoFactura
+            // 
+            this.cmbTipoFactura.FormattingEnabled = true;
+            this.cmbTipoFactura.Location = new System.Drawing.Point(124, 25);
+            this.cmbTipoFactura.Name = "cmbTipoFactura";
+            this.cmbTipoFactura.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoFactura.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Fecha";
+            // 
+            // pickerFechaFactura
+            // 
+            this.pickerFechaFactura.Location = new System.Drawing.Point(254, 26);
+            this.pickerFechaFactura.Name = "pickerFechaFactura";
+            this.pickerFechaFactura.Size = new System.Drawing.Size(200, 20);
+            this.pickerFechaFactura.TabIndex = 3;
+            this.pickerFechaFactura.Value = new System.DateTime(2019, 10, 1, 18, 55, 12, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Numero de Estadia";
+            // 
+            // txtNroEstadia
+            // 
+            this.txtNroEstadia.Location = new System.Drawing.Point(251, 76);
+            this.txtNroEstadia.Name = "txtNroEstadia";
+            this.txtNroEstadia.Size = new System.Drawing.Size(100, 20);
+            this.txtNroEstadia.TabIndex = 0;
+            this.txtNroEstadia.Text = "0";
+            // 
+            // txtNroFactura
+            // 
+            this.txtNroFactura.Location = new System.Drawing.Point(15, 25);
+            this.txtNroFactura.Name = "txtNroFactura";
+            this.txtNroFactura.Size = new System.Drawing.Size(100, 20);
+            this.txtNroFactura.TabIndex = 0;
+            this.txtNroFactura.Text = "0";
+            // 
+            // pickerFechaInicioEstadia
+            // 
+            this.pickerFechaInicioEstadia.Location = new System.Drawing.Point(15, 76);
+            this.pickerFechaInicioEstadia.Name = "pickerFechaInicioEstadia";
+            this.pickerFechaInicioEstadia.Size = new System.Drawing.Size(230, 20);
+            this.pickerFechaInicioEstadia.TabIndex = 3;
+            this.pickerFechaInicioEstadia.Value = new System.DateTime(2019, 10, 1, 18, 55, 12, 0);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fecha Inicio Estadia";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(357, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "(Se completa solo, mostrando que encontro la estadia)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(354, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Cliente";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_articulo,
+            this.nombre,
+            this.descripcion,
+            this.precioUnitario});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(611, 257);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // precioUnitario
+            // 
+            this.precioUnitario.Frozen = true;
+            this.precioUnitario.HeaderText = "Precio";
+            this.precioUnitario.Name = "precioUnitario";
+            // 
+            // descripcion
+            // 
+            this.descripcion.Frozen = true;
+            this.descripcion.HeaderText = "Descripción";
+            this.descripcion.Name = "descripcion";
+            // 
+            // nombre
+            // 
+            this.nombre.Frozen = true;
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // id_articulo
+            // 
+            this.id_articulo.Frozen = true;
+            this.id_articulo.HeaderText = "ID";
+            this.id_articulo.Name = "id_articulo";
+            // 
+            // frmFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pickerFechaInicioEstadia);
+            this.Controls.Add(this.pickerFechaFactura);
+            this.Controls.Add(this.cmbTipoFactura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNroEstadia);
+            this.Controls.Add(this.txtNroFactura);
+            this.Name = "frmFactura";
+            this.Text = "frmFactura";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipoFactura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker pickerFechaFactura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNroEstadia;
+        private System.Windows.Forms.TextBox txtNroFactura;
+        private System.Windows.Forms.DateTimePicker pickerFechaInicioEstadia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_articulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioUnitario;
+    }
+}
